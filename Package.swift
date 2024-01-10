0eadc49ab7029f23ca211990ce3affc1b160d765
Daniela Hengl <d.hengl@icloud.com>
2024-01-10 11:02:10 +0100

    - Initial commit: Add contents of MacroTester Macro

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let name = String(#filePath.split(separator: "/").reversed()[1])
+
+let package = Package(
+  name: name,
+  platforms: [.macOS(.v12), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+  products: [
+    .library(
+      name: name,
+      targets: [name]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0")
+  ],
+  targets: [
+    .target(
+      name: name,
+      dependencies: [
+        .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+        .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+      ]
+    )
+  ]
+)
