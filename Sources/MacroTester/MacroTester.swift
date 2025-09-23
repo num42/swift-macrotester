86cba4c87d4ae7d4f179691bdd55b709f6922c6f
patrickavs <patrick-rc-pfeil@gmx.de>
2025-09-23 16:06:00 +0200

    update testMacro for swift testing

--- Sources/MacroTester/MacroTester.swift
+++ Sources/MacroTester/MacroTester.swift
@@ -3,27 +3,35 @@ import SwiftSyntaxMacros
 import SwiftSyntaxMacrosTestSupport
 
 public struct MacroTester {
-  public static func testMacro(test: String = #function, filePath: String = #filePath, macros: [String: Macro.Type]) {
-    let testName = test.replacingOccurrences(of: "()", with: "")
+    public static func testMacro(
+        testName: String,
+        filePath: String = #filePath,
+        macros: [String: Macro.Type]
+    ) {
+        assertMacroExpansion(
+            inputSourceCode(for: testName, filePath: filePath),
+            expandedSource: outputSourceCode(for: testName, filePath: filePath),
+            macros: macros
+        )
 
-    assertMacroExpansion(
-      inputSourceCode(for: testName, filePath: filePath),
-      expandedSource: outputSourceCode(for: testName, filePath: filePath),
-      macros: macros
-    )
-  }
+        assertMacroExpansion(
+            inputSourceCode(for: testName, filePath: filePath),
+            expandedSource: outputSourceCode(for: testName, filePath: filePath),
+            macros: macros
+        )
+    }
 
-  private static func inputSourceCode(for test: String, filePath: String) -> String {
-    sourceCode(fileName: "Input", test: test, filePath: filePath)
-  }
+    private static func inputSourceCode(for test: String, filePath: String) -> String {
+        sourceCode(fileName: "Input", test: test, filePath: filePath)
+    }
 
-  private static func outputSourceCode(for test: String, filePath: String) -> String {
-    sourceCode(fileName: "Output", test: test, filePath: filePath)
-  }
+    private static func outputSourceCode(for test: String, filePath: String) -> String {
+        sourceCode(fileName: "Output", test: test, filePath: filePath)
+    }
 
-  private static func sourceCode(fileName: String, test: String, filePath: String) -> String {
-    try! String(
-      contentsOf: URL(fileURLWithPath: filePath).deletingLastPathComponent().appendingPathComponent("Resources").appendingPathComponent(test).appendingPathComponent("\(fileName).swift.test")
-    )
-  }
+    private static func sourceCode(fileName: String, test: String, filePath: String) -> String {
+        try! String(
+            contentsOf: URL(fileURLWithPath: filePath).deletingLastPathComponent().appendingPathComponent("Resources").appendingPathComponent(test).appendingPathComponent("\(fileName).swift.test")
+        )
+    }
 }
