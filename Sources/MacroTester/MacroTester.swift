2678b49c44d86a9746ad1252547e320b846ed6b0
Wolfgang Lutz <wlut@num42.de>
2025-09-26 16:51:26 +0200

    run “swift format --recursive --in-place .”

--- Sources/MacroTester/MacroTester.swift
+++ Sources/MacroTester/MacroTester.swift
@@ -3,7 +3,9 @@ import SwiftSyntaxMacros
 import SwiftSyntaxMacrosTestSupport
 
 public struct MacroTester {
-  public static func testMacro(test: String = #function, filePath: String = #filePath, macros: [String: Macro.Type]) {
+  public static func testMacro(
+    test: String = #function, filePath: String = #filePath, macros: [String: Macro.Type]
+  ) {
     let testName = test.replacingOccurrences(of: "()", with: "")
 
     assertMacroExpansion(
@@ -23,7 +25,11 @@ public struct MacroTester {
 
   private static func sourceCode(fileName: String, test: String, filePath: String) -> String {
     try! String(
-      contentsOf: URL(fileURLWithPath: filePath).deletingLastPathComponent().appendingPathComponent("Resources").appendingPathComponent(test).appendingPathComponent("\(fileName).swift.test")
+      contentsOf: URL(fileURLWithPath: filePath)
+        .deletingLastPathComponent()
+        .appendingPathComponent("Resources")
+        .appendingPathComponent(test)
+        .appendingPathComponent("\(fileName).swift.test")
     )
   }
 }
